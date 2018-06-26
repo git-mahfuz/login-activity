8d224ee26c628a994990ca79d9ed8a7713e711f1
Mahfuz <mahfuzcse12@gmail.com>
2018-06-26 17:12:16 +0600

    Issue with Auth Fixed

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::group(['namespace' => 'Mahfuz\LoginActivity\Http\Controllers'], function() {
+Route::group(['namespace' => 'Mahfuz\LoginActivity\Http\Controllers', 'middleware' => ['web', 'auth']], function() {
     Route::get('login-activity', 'LoginActivityController@index');
 });
